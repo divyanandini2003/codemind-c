4dadc7ec69e1e0d62b8dbd782b1238fe00149bef
divyanandini2003 <97425770+divyanandini2003@users.noreply.github.com>
2022-04-23 07:43:28 +0530

    #include<stdio.h> int main() {     int n,s=0,p=1,r;     scanf("%d",&n);     while(n>0)     {         r=n%10;         s=s+r;         p=p*r;         n=n/10;     }     if(s==p)     {         printf("Spy Number");     }     else     {         printf("Not Spy Number");     } }

--- Spy_Number.c
+++ Spy_Number.c
@@ -1,21 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int n,sum=0,pro=1,r;
+    int n,s=0,p=1,r;
     scanf("%d",&n);
     while(n>0)
     {
         r=n%10;
-        pro=pro*r;
-        sum=sum+r;
+        s=s+r;
+        p=p*r;
         n=n/10;
-        
     }
-    if(sum==pro)
+    if(s==p)
     {
         printf("Spy Number");
     }
-    else 
+    else
     {
         printf("Not Spy Number");
     }
