5bf78006ad8b316aa99726717cab0d1e38ed9110
divyanandini2003 <97425770+divyanandini2003@users.noreply.github.com>
2022-04-26 19:39:29 +0530

    #include<stdio.h> int main() {     int n,i,flag=0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         if(i*(i+1)==n)         {         flag=1;         break;         }     }     if(flag==1)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Pronic_Number_or_Heteromecic_Number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,flag=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        if(i*(i+1)==n)
+        {
+        flag=1;
+        break;
+        }
+    }
+    if(flag==1)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
