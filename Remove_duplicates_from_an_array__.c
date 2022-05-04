0af4398f7b2abca3d608128111fc5ba71389871f
divyanandini2003 <97425770+divyanandini2003@users.noreply.github.com>
2022-05-04 21:56:42 +0530

    #include<stdio.h> int main() {     int n,a[100],i,j;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         for(j=0;j<n;j++)         {             if(a[i]==a[j] && i!=j)             {                 a[j]=-1;             }         }     }     for(i=0;i<n;i++)     {         if(a[i]!=-1)         {             printf("%d ",a[i]);         }     } }

---
+++ Remove_duplicates_from_an_array__.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(a[i]==a[j] && i!=j)
+            {
+                a[j]=-1;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]!=-1)
+        {
+            printf("%d ",a[i]);
+        }
+    }
+}
