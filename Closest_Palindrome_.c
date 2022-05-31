bb6cf385bededd7c82fb6bbdf3b931de42b47cc5
divyanandini2003 <97425770+divyanandini2003@users.noreply.github.com>
2022-05-31 07:19:48 +0530

    #include<stdio.h> int main() {     int n,i,j,r,cp,sp,d,e;     scanf("%d",&n);     for(i=n-1;i>=n-20;i--)     {         j=i;         r=0;         while(j>0)         {             r=r*10+j%10;             j/=10;         }         if(r==i)         {             cp=i;             break;         }     }     for(i=n+1;i<=n+20;i++)     {         j=i;         r=0;         while(j>0)         {             r=r*10+j%10;             j/=10;         }         if(r==i)         {             sp=i;             break;         }     }     d=n-cp;     e=sp-n;     if(d<e)     {         printf("%d",cp);     }     else if(d==e)     {         printf("%d %d",cp,sp);     }     else     {         printf("%d",sp);     } }

---
+++ Closest_Palindrome_.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,r,cp,sp,d,e;
+    scanf("%d",&n);
+    for(i=n-1;i>=n-20;i--)
+    {
+        j=i;
+        r=0;
+        while(j>0)
+        {
+            r=r*10+j%10;
+            j/=10;
+        }
+        if(r==i)
+        {
+            cp=i;
+            break;
+        }
+    }
+    for(i=n+1;i<=n+20;i++)
+    {
+        j=i;
+        r=0;
+        while(j>0)
+        {
+            r=r*10+j%10;
+            j/=10;
+        }
+        if(r==i)
+        {
+            sp=i;
+            break;
+        }
+    }
+    d=n-cp;
+    e=sp-n;
+    if(d<e)
+    {
+        printf("%d",cp);
+    }
+    else if(d==e)
+    {
+        printf("%d %d",cp,sp);
+    }
+    else
+    {
+        printf("%d",sp);
+    }
+}
