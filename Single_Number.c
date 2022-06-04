4759a031b032d254e1991a3ddde94cb2144401dd
divyanandini2003 <97425770+divyanandini2003@users.noreply.github.com>
2022-06-04 19:43:37 +0530

    #include<stdio.h> int main() {     int n,a[n],i,j,flag=0,s=0;     scanf("%d",&n);     for (i=0;i<=n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<=n;i++)     {         flag=0;
            for(j=0;j<=n;j++)
            {
                if(a[i]==a[j] && i!=j)
                {
                    flag=1;
                }
            }
            if(flag==0)
            {
                s+=a[i];
            }
        }
        printf("%d",s);
    }

---
+++ Single_Number.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[n],i,j,flag=0,s=0;
+    scanf("%d",&n);
+    for (i=0;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<=n;i++)
+    {
+        flag=0;
+        
+        for(j=0;j<=n;j++)
+        {
+            if(a[i]==a[j] && i!=j)
+            {
+                flag=1;
+            }
+        }
+        if(flag==0)
+        {
+            s+=a[i];
+        }
+    }
+    printf("%d",s);
+}
