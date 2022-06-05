14cd606cf9693076717103dd3e907d770e7092fc
divyanandini2003 <97425770+divyanandini2003@users.noreply.github.com>
2022-06-05 14:43:43 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if(j==1||i==n||i==j)             {                 printf("*");             }             else             {                 printf(" ");             }         }         printf(" ");     }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -5,11 +5,19 @@ int main()
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        for(j=1;j<=i;j++)
+        for(j=1;j<=n;j++)
         {
-            printf("%d",j);
+            if(j==1||i==n||i==j)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
         }
         printf("
 ");
     }
+    
 }
