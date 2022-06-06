fa52b233798fbe1e65308b77ab9da63077c0a84d
divyanandini2003 <97425770+divyanandini2003@users.noreply.github.com>
2022-06-06 14:20:54 +0530

    #include<stdio.h> int main() {     int n,se,i,c=0;     scanf("%d%d",&n,&se);     int a[n];     for(i=0;i<=n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%se!=0)         {             c++;         }     }     printf("%d",c); }

---
+++ Count_elements_not_divisible_by_K.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,se,i,c=0;
+    scanf("%d%d",&n,&se);
+    int a[n];
+    for(i=0;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%se!=0)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
