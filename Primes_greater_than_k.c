096b0d806931f04d9390a5f2d608155b58dec739
divyanandini2003 <97425770+divyanandini2003@users.noreply.github.com>
2022-06-07 11:17:05 +0530

    #include<stdio.h> int prime(int n) {     int i,c=0;     for(i=2;i<=n/2;i++)     {         if(n%i==0)         {             c++;         }     }     if(c>0||n==1)     {         return 1;     }     else     {         return 0;     } } int main() {     int n;     scanf("%d",&n);     int a[n],i,k,c=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     scanf("%d",&k);     for(i=0;i<n;i++)     {         if(a[i]>=k)         {             if(prime(a[i])==0)             {                 c++;             }         }     }     printf("%d",c); }

---
+++ Primes_greater_than_k.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int prime(int n)
+{
+    int i,c=0;
+    for(i=2;i<=n/2;i++)
+    {
+        if(n%i==0)
+        {
+            c++;
+        }
+    }
+    if(c>0||n==1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i,k,c=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d",&k);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>=k)
+        {
+            if(prime(a[i])==0)
+            {
+                c++;
+            }
+        }
+    }
+    printf("%d",c);
+}
